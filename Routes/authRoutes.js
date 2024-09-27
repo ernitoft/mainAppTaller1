d9cec2e11b87761039ba57580b01464443c4c4c1
ernitoft <ernes.fuenzalida@alumnos.ucn.cl>
2024-09-26 23:18:35 -0300

    Se añade la ruta del inicio como predeterminada, un GET de aviso.

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -4,5 +4,8 @@ const router = Router();
 const { login } = require("../Controllers/authController");
 
 router.post( '/login',login);
+router.get('/', (req, res)=> {
+    res.send('Taller 1 Iniciado');
+})
 
 module.exports = router;
